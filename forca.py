b01abfb559217cfa53f936d5a2e931363522673c
Lucas Machado <lucasmvil@hotmail.com>
2022-08-18 10:12:23 -0300

    Preparando o jogo da forca

--- forca.py
+++ forca.py
@@ -3,6 +3,14 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while (not acertou and not enforcou):
+        print("Jogando...")
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
