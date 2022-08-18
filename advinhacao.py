13cda38ba01959695f4a9934d8e5d44fc164271b
Lucas Machado <lucasmvil@hotmail.com>
2022-08-18 08:06:37 -0300

    A sequência do jogo

--- advinhacao.py
+++ advinhacao.py
@@ -3,19 +3,25 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(input("Digite o seu número: "))
+while (rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = int(input("Digite o seu número: "))
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if(acertou):
-    print("Parabéns! Você acertou!")
-else:
-    if(maior):
-        print("O seu chute foi maior do que o número secreto!")
-    elif(menor):
-        print("O seu chute foi menor do que o número secreto!")
+    if(acertou):
+        print("Parabéns! Você acertou!")
+    else:
+        if(maior):
+            print("O seu chute foi maior do que o número secreto!")
+        elif(menor):
+            print("O seu chute foi menor do que o número secreto!")
+
+    rodada += 1
 
 print("Fim do jogo")
