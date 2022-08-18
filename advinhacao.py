98694acc3570f2fd8ab4d8eb2f5070e4bb909cac
Lucas Machado <lucasmvil@hotmail.com>
2022-08-18 07:48:14 -0300

    Testando valores

--- advinhacao.py
+++ advinhacao.py
@@ -4,13 +4,18 @@ print("*********************************")
 
 numero_secreto = 42
 
-chute_str = input("Digite o seu número: ")
-print("Você digitou: ", chute_str)
-chute = int(chute_str)
+chute = int(input("Digite o seu número: "))
 
-if (numero_secreto == chute):
-    print("Você acertou!")
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if(acertou):
+    print("Parabéns! Você acertou!")
 else:
-    print("Você errou!")
+    if(maior):
+        print("O seu chute foi maior do que o número secreto!")
+    elif(menor):
+        print("O seu chute foi menor do que o número secreto!")
 
 print("Fim do jogo")
